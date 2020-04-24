9e14bb92cccdf75082abeb0a6788218f7fbc9909
Ji Zhou <calf.zhou@gmail.com>
2020-04-25 01:45:38 +0800

    feat(v2rayN): add gen-v.sh to generate v2rayN/G subscription content

---
+++ gen-v.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+shopt -s nullglob
+
+for protocol in vmess ss socks; do
+    for f in ${protocol}*.json; do
+        echo ${protocol}://$(base64 -w 0 "$f")
+    done
+done | base64 -w 0
+
+echo
